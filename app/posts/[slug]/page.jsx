8d7843f7b6c74ff04660be4e2ddfb8ffb29d459d
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-04-15 12:58:06 -0400

    posts md

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -1,16 +1,32 @@
 import { getPostBySlug } from '@/lib/mdx-utils'
 import Link from 'next/link'
+import { useMDXComponents } from '@/mdx-component'
 export default async function Page({ params }) {
     const { meta, content } = await getPostBySlug(params.slug)
     console.log(meta)
     return (
-        <section className="w-full border-2">
-            <div>
-                <Link href="/">Back to home</Link>
+        <>
+            <div className="flex flex-col gap-2 font-inter">
+                <div className="flex gap-4">
+                    <Link href="/">
+                        <p className="text-sm text-black-700">{`Home`}</p>
+                    </Link>
+                    <Link href="/posts">
+                        <p className="text-sm text-black-700">{`All posts`}</p>
+                    </Link>
+                </div>
+                <div>
+                    <h1 className="font-syne text-2xl font-semibold">
+                        {meta.title}
+                    </h1>
+                    <p className="text-sm">Published @ {meta.date}</p>
+                </div>
             </div>
-            <h1>{meta.title}</h1>
-            <p>{meta.publishedDate}</p>
-            {/* <article>{content}</article> */}
-        </section>
+            <section className="flex flex-col gap-3 font-inter">
+                <div className="prose max-w-none text-white">
+                  {content}
+                  </div>
+            </section>
+        </>
     )
 }

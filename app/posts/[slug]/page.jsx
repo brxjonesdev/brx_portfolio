5df9ff21fb01605b7d935fec52c969a635461da1
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-04-16 00:05:24 -0400

    ready for deploy!

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -1,9 +1,10 @@
 import { getPostBySlug } from '@/lib/mdx-utils'
 import Link from 'next/link'
 import { useMDXComponents } from '@/mdx-component'
+import { Separator } from '@/components/ui/separator'
+
 export default async function Page({ params }) {
     const { meta, content } = await getPostBySlug(params.slug)
-    console.log(meta)
     return (
         <>
             <div className="flex flex-col gap-2 font-inter">
@@ -19,13 +20,22 @@ export default async function Page({ params }) {
                     <h1 className="font-syne text-2xl font-semibold">
                         {meta.title}
                     </h1>
-                    <p className="text-sm">Published @ {meta.date}</p>
+                    <p className="text-sm">Published // {meta.date}</p>
                 </div>
             </div>
-            <section className="flex flex-col gap-3 font-inter">
-                <div className="prose max-w-none text-white">
-                  {content}
-                  </div>
+            <Separator />
+            <section className="flex-colfont-inter flex">
+                <div
+                    className="
+                prose-heading
+                prose
+                text-white prose-headings:font-syne prose-headings:text-2xl prose-headings:text-black-800
+                                prose-a:text-mauve-500 prose-a:underline prose-a:hover:text-mauve-600
+                                prose-a:font-inter prose-a:text-sm
+                "
+                >
+                    {content}
+                </div>
             </section>
         </>
     )
--- mdx-component.js
+++ mdx-component.js
@@ -2,7 +2,7 @@ export function useMDXComponents(components) {
     return {
         h1: ({ children }) => (
             <h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>
-          ),
-          ...components
+        ),
+        ...components,
     }
-  }
+}

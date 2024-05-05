b8499d8f7696df9369150ee7c774eb7be2df0368
Braxton <Braxtonjonesttsl@gmail.com>
2024-05-05 01:02:35 -0400

    Update file path in mdx-utils.jsx

--- components/brx/project.jsx
+++ components/brx/project.jsx
@@ -5,7 +5,7 @@ export default function Project({ name, description, link, color }) {
     return (
         <Link href={link}>
             <div className="flex items-center gap-5 rounded-md bg-black-200 p-4 hover:bg-black-400">
-                <div className="flex h-14 w-2 items-center justify-center rounded-md bg-mauve"></div>
+                <div className="flex h-14 min-w-2 items-center justify-center rounded-md bg-mauve"></div>
                 <div className="flex flex-col gap-1">
                     <h1 className="font-syne font-semibold text-mauve">
                         {name}
